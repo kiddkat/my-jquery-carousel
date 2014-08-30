bcabd13b66b16a29f993430de803f373a9218f04
kiddkat <alisonjeng@gmail.com>
2014-08-29 20:30:21 -0400

    added arrow position and show hide on arrows and finishing styles

--- js/carousel.js
+++ js/carousel.js
@@ -6,10 +6,20 @@ $(function(){
 
   $('#slide_tray').width(slideWidth * slideCount);
 
+  $('#prev, #next').hide();
+
+  $('#carousel').hover(function (){
+      $('#prev, #next').show();
+
+  }, function() {
+      $('#prev, #next').hide();
+
+  });
+
+
   $('#next').click(function(){
     if (pos < slideCount - 1) {
       pos += 1; //increments position by 1 (800px x 1)
-
     } else {
       pos = 0;
     }
@@ -21,9 +31,9 @@ $(function(){
   $('#prev').click(function(){
     if (pos > 0) {
         pos-=1;
-      } else {
-        pos = slideCount - 1;
-      }
+    } else {
+      pos = slideCount - 1;
+    }
 
       slideAmount = slideWidth * pos;
     $('#slide_tray').animate({'right': slideAmount},400);
