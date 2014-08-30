52acf820375a6b4fa3cfdefa15a6f6e0f4762db9
kiddkat <alisonjeng@gmail.com>
2014-08-29 20:00:15 -0400

    working carousel

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+$(function(){
+
+  var slideWidth = $('.slide').width(),
+      slideCount = $('.slide').size(),
+      pos = 0;
+
+  $('#slide_tray').width(slideWidth * slideCount);
+
+  $('#next').click(function(){
+    if (pos < slideCount - 1) {
+      pos += 1; //increments position by 1 (800px x 1)
+
+    } else {
+      pos = 0;
+    }
+      var slideAmount = slideWidth * pos;
+      $('#slide_tray').animate({'right': slideAmount},400);  
+    });
+
+
+  $('#prev').click(function(){
+    if (pos > 0) {
+        pos-=1;
+      } else {
+        pos = slideCount - 1;
+      }
+
+      slideAmount = slideWidth * pos;
+    $('#slide_tray').animate({'right': slideAmount},400);
+  });
+
+
+});
